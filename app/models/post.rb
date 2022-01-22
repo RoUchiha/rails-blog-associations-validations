dca0f8de66d62897cf9e4f600c7c4972a49eb0de
RoUchiha <swaggsikh@gmail.com>
2022-01-22 16:33:13 -0600

    commit tree 51feea50d0e4ac8533238691bca6c6a0bd7c67ad

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,10 @@
 class Post < ActiveRecord::Base
+    validates :name, presence: true
+    validates :content, presence: true 
+
+    has_many :post_tags
+    belongs_to :user
+    has_many :tags, through: :post_tags
+    
   
 end
